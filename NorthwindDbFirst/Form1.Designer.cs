88cdb62f91a2303d1d6a500b662acac083c62515
Yavuz Özbay <ozbayavz@gmail.com>
2023-07-15 00:21:18 +0200

    "Employees names in database are listed !"

---
+++ NorthwindDbFirst/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace NorthwindDbFirst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstEmployee = new System.Windows.Forms.ListBox();
+            this.btnList = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.FirstName = new System.Windows.Forms.Label();
+            this.lastName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstEmployee
+            // 
+            this.lstEmployee.FormattingEnabled = true;
+            this.lstEmployee.Location = new System.Drawing.Point(85, 57);
+            this.lstEmployee.Name = "lstEmployee";
+            this.lstEmployee.Size = new System.Drawing.Size(158, 316);
+            this.lstEmployee.TabIndex = 0;
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(389, 84);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(184, 57);
+            this.btnList.TabIndex = 1;
+            this.btnList.Text = "List";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(389, 296);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(184, 49);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(389, 164);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(389, 213);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(184, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // FirstName
+            // 
+            this.FirstName.AutoSize = true;
+            this.FirstName.Location = new System.Drawing.Point(318, 164);
+            this.FirstName.Name = "FirstName";
+            this.FirstName.Size = new System.Drawing.Size(54, 13);
+            this.FirstName.TabIndex = 5;
+            this.FirstName.Text = "FirstName";
+            // 
+            // lastName
+            // 
+            this.lastName.AutoSize = true;
+            this.lastName.Location = new System.Drawing.Point(318, 220);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(55, 13);
+            this.lastName.TabIndex = 6;
+            this.lastName.Text = "LastName";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lastName);
+            this.Controls.Add(this.FirstName);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.lstEmployee);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstEmployee;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label FirstName;
+        private System.Windows.Forms.Label lastName;
+    }
+}
+
---
+++ NorthwindDbFirst/Form1.cs
@@ -0,0 +1,39 @@
+﻿using NorthwindDbFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NorthwindDbFirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        NorthwindEntities db = new NorthwindEntities();
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            List();
+        }
+
+        private void List()
+        {
+            lstEmployee.DataSource = db.Employees.ToList();
+            lstEmployee.DisplayMember = "FirstName";
+            lstEmployee.SelectedIndex = -1;
+        }
+    }
+}
