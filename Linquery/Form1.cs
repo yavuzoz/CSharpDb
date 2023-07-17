3cd0ebf27d76f2bc88211b153c0d40f759f5f71f
Yavuz Özbay <ozbayavz@gmail.com>
2023-07-17 14:42:43 +0200

    "for Linquery datagridwiev in form added "

---
+++ Linquery/Form1.cs
@@ -0,0 +1,35 @@
+﻿using Linquery.Models;
+using Linquery.SingletonPattern;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Linquery
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //LINQ(languege integrated query)
+
+        #region Question 1
+        // list all the data in the products table.
+        #endregion
+
+        NorthwindEntities db = DBTool.DbInstance;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvProducts.DataSource = db.Products.ToList();
+        }
+    }
+}
---
+++ Linquery/SingletonPattern/DBTool.cs
@@ -0,0 +1,28 @@
+﻿using Linquery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linquery.SingletonPattern
+{
+    class DBTool
+    {
+        private DBTool() { }
+        
+        private static NorthwindEntities _dbInstance;
+
+        public static NorthwindEntities DbInstance
+        {
+            get
+            {
+                if (_dbInstance == null)
+                {
+                    _dbInstance = new NorthwindEntities();
+                }
+                return _dbInstance;
+            }
+        }
+    }
+}
